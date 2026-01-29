996177b20cd0835bdae614134cc046133a66b27b
3zs <3zs.dev1ng@gmail.com>
2026-01-29 10:42:52 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+export function activate({ importCSS }) {
+  const customCSS = `
+    .lyrics-panel .lyric-line.past.near,
+    .lyrics-panel .lyric-line.past.mid,
+    .lyrics-panel .lyric-line.past.far {
+      filter: none !important;
+      opacity: 1 !important;
+    }
+    [data-theme=light] .lyrics-panel .lyric-line.past.near {
+      color: rgba(0, 0, 0, 0.45) !important;
+    }
+    [data-theme=light] .lyrics-panel .lyric-line.past.mid {
+      color: rgba(0, 0, 0, 0.3) !important;
+    }
+    [data-theme=light] .lyrics-panel .lyric-line.past.far {
+      color: rgba(0, 0, 0, 0.2) !important;
+    }
+
+    [data-theme=dark] .lyrics-panel .lyric-line.past.near {
+      color: rgba(255, 255, 255, 0.45) !important;
+    }
+    [data-theme=dark] .lyrics-panel .lyric-line.past.mid {
+      color: rgba(255, 255, 255, 0.3) !important;
+    }
+    [data-theme=dark] .lyrics-panel .lyric-line.past.far {
+      color: rgba(255, 255, 255, 0.2) !important;
+    }
+  `;
+  importCSS(customCSS);
+}
+
+export function deactivate() {}
